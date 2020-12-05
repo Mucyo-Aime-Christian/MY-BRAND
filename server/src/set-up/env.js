fa2999b37a418e2a178752b1fe1b960933ed9e68
Mucyo-Aime-Christian <72758441+Mucyo-Aime-Christian@users.noreply.github.com>
2020-12-05 13:26:44 +0200

    resolving conflicts

--- server/src/set-up/env.js
+++ server/src/set-up/env.js
@@ -1,8 +1,8 @@
 import { config } from "dotenv";
 config();
-export const PORT= process.env.PORT
-export const JWT_KEY = process.env.JWT_KEY
-export const NODE_ENV = process.env.NODE_ENV
+export const PORT= process.env.PORT;
+export const JWT_KEY = process.env.JWT_KEY;
+export const NODE_ENV = process.env.NODE_ENV;
 export const DATABASE_URL =
   process.env.NODE_ENV !== "test"
     ? process.env.DATABASE_URL
